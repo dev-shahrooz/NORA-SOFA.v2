c68e7ea601b11e743ab451a5097c84c26a6381e2
Shahrouz-Ebrahimi <shahrooz.cto@gmail.com>
2025-09-08 13:24:43 +0330

    debug audio module

--- core/usecases/audio.py
+++ core/usecases/audio.py
@@ -11,5 +11,4 @@ class AudioUsecase:
     def set_volume(self, volume: int) -> Dict:
         volume = max(0, min(100, int(volume)))
         self.svc.set_volume(volume)
-        current = self.svc.get_volume()
         return {"audio": {"volume": current}}
