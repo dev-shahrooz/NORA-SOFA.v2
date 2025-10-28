1be8b532690e8f28eb2c44ab4e29c99f639669a8
Parsa <sepasiparsa@gmail.com>
2025-10-28 14:44:20 +0330

    update service backend & bt-pair & kiosk and  shel bt-pair

--- scripts/nora-bt-pair.sh
+++ scripts/nora-bt-pair.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 set -e
-TIMEOUT=${1:-120}
 bluetoothctl <<EOF
 power on
-agent NoInputNoOutput
-default-agent
 system-alias "Nora Sofa"
 disconnect
 discoverable on
 pairable on
-EOF
-sleep "$TIMEOUT"
-bluetoothctl <<EOF
-discoverable off
-pairable off
-scan off
 EOF
